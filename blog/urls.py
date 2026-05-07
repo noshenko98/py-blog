53d893187f53b7f12958a33d0ed7f1f5050259fb
noshenko98 <noshenko98@gmail.com>
2026-05-07 16:38:06 +0300

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,8 @@ from blog.views import PostDetailView, PostList
 
 urlpatterns = [
     path("", PostList.as_view(), name="index"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail")
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, ListView
+from django.shortcuts import redirect
 
-from blog.models import Post, Commentary
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import FormMixin
 
+from blog.forms import CommentaryForm
+from blog.models import Post
 
-# def index(request):
-#     post_list = Post.objects.all().order_by('-created_time')
-#     paginator = Paginator(post_list, 5)
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     context_object_name = "post_list"
-#     context = {"posts": page_obj,
-#                }
-#     return render(request, "blog/index.html", context=context)
 
 class PostList(LoginRequiredMixin, ListView):
     model = Post
@@ -23,12 +14,24 @@ class PostList(LoginRequiredMixin, ListView):
     context_object_name = "post_list"
 
 
-class PostDetailView(DetailView):
+class PostDetailView(FormMixin, DetailView):
     model = Post
+    form_class = CommentaryForm
 
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect("login")
 
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Commentary
-    fields = ["content"]
-    template_name = "blog/post_detail.html"
-    success_url = reverse_lazy("blog:post-detail", kwargs={"pk" : 1})
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.user = self.request.user
+        comment.save()
+        return redirect("blog:post-detail", pk=self.object.pk)
